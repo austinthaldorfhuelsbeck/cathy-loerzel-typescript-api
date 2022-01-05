619df5f41a6b5d7334fa2c2dbd7fc77adcfcbbaf
austinthaldorfhuelsbeck <austinthaldorfhuelsbeck@gmail.com>
2022-01-05 13:20:27 -0800

    events integer increment fix

--- src/db/migrations/20210902125132_createEvents.js
+++ src/db/migrations/20210902125132_createEvents.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable("events", (table) => {
-    table.integer("event_id").primary()
+    table.increments("event_id").primary()
     table.string("name")
     table.string("date")
     table.string("content", 10000)
