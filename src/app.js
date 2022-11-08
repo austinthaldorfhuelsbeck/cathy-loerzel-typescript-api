05e927375d87cbcf4c0b6c574a8bba2317221b30
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-11-07 22:44:01 -0800

    supertokens init

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 // Import Dependencies
 const express = require('express');
 const cors = require('cors');
+const dotenv = require("dotenv");
+const supertokens = require('supertokens-node');
+const { middleware } = require("supertokens-node/framework/express");
+const { errorHandler } = require("supertokens-node/framework/express");
 // Import Route handlers
 const blogsRoute = require('./routes/blogsRoute');
 const eventsRoute = require('./routes/eventsRoute');
 const testimonialsRoute = require('./routes/testimonialsRoute');
 // Import Error handlers
 const notFound = require('./errors/notFound');
-const errorHandler = require('./errors/errorHandler');
+const errorHandlerGeneric = require('./errors/errorHandler');
+const { error } = require('console');
+
+// Utilize environment variables
+dotenv.config();
 
 // Define app
 const app = express();
@@ -15,9 +23,12 @@ const app = express();
 // Middleware for CORS and Express
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.DASHBOARD_BASE_URL,
+    allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+    credentials: true,
   })
 );
+app.use(middleware());
 app.use(express.json());
 
 // Route handlers
@@ -25,9 +36,10 @@ app.use('/blogs', blogsRoute);
 app.use('/events', eventsRoute);
 app.use('/testimonials', testimonialsRoute);
 
-// Eroor handlers
+// Error handlers
 app.use(notFound);
 app.use(errorHandler);
+app.use(errorHandlerGeneric);
 
 // Export module
 module.exports = app;
